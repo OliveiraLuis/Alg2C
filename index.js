1d878c3441fb1ae19e080358d76855035e9e1aed
OliveiraLuis <oliveira1eduardo2luis3@gmail.com>
2019-01-23 20:37:00 -0200

    Correção dos comentários

--- index.js
+++ index.js
@@ -29,77 +29,81 @@ window.onload = function () {
 }
 
 /**
- * Função para encontrar a 'ocorrencia' e substituir pela 'substituta'
+ * Chamada das funções para tratar do texto de entrada
  *
  */
-var mudaArquivoparaC = function (frase) {
-    frase = adicionaAbreChaves(frase);
-    frase = adicionaFechaChaves(frase);
-    frase = adicionaTiposDeVariaveis(frase);
-    frase = adicionaAtribuidores(frase);
-    frase = removerFrasesInutesis(frase);
-    return frase;
+var mudaArquivoparaC = function (texto) {
+    texto = adicionaAbreChaves(texto);
+    texto = adicionaFechaChaves(texto);
+    texto = adicionaTiposDeVariaveis(texto);
+    texto = adicionaAtribuidores(texto);
+    texto = removerFrasesInutesis(texto);
+    return texto;
 }
 
 /**
- * Função para encontrar a 'ocorrencia' e substituir pela 'substituta'
+ * Função para encontrar a 'ocorrencia' no 'texto' e substituir pela 'substituta'
  *
  */
-var substituiOcorrencia = function (frase, ocorrencia, substituta) {
-    return frase.replace(ocorrencia, substituta);
+var substituiOcorrencia = function (texto, ocorrencia, substituta) {
+    return texto.replace(ocorrencia, substituta);
 }
 
 /**
  * Função para colocar o '{' nos lugares apropriados
  *
  */
-var adicionaAbreChaves = function (frase) {
+var adicionaAbreChaves = function (texto) {
     caractere = '{';
-    frase = substituiOcorrencia(frase, /inicio/g, caractere);
-    frase = substituiOcorrencia(frase, /entao/g, caractere);
-    frase = substituiOcorrencia(frase, /faca/g, caractere);
-    return frase;
+    texto = substituiOcorrencia(texto, /inicio/g, caractere);
+    texto = substituiOcorrencia(texto, /entao/g, caractere);
+    texto = substituiOcorrencia(texto, /faca/g, caractere);
+    return texto;
 }
 
 /**
  * Função para colocar o '}' nos lugares apropriados
  *
  */
-var adicionaFechaChaves = function (frase) {
+var adicionaFechaChaves = function (texto) {
     caractere = '}';
-    frase = substituiOcorrencia(frase, /fim;/g, caractere);
-    frase = substituiOcorrencia(frase, /fim-enquanto;/g, caractere);
-    frase = substituiOcorrencia(frase, /senao/g, caractere + 'senao');
-    return frase;
+    texto = substituiOcorrencia(texto, /fim;/g, caractere);
+    texto = substituiOcorrencia(texto, /fim-enquanto;/g, caractere);
+    texto = substituiOcorrencia(texto, /senao/g, caractere + 'senao');
+    return texto;
 }
 
 /**
- * Função para colocar os nomes das váriaveis
+ * Função para colocar os nomes dos tipos das váriaveis
  *
  */
-var adicionaTiposDeVariaveis = function (frase) {
-    frase = substituiOcorrencia(frase, /inteiro:/g, 'int');
-    frase = substituiOcorrencia(frase, /real:/g, 'float');
-    frase = substituiOcorrencia(frase, /caractere:/g, 'char');
-    frase = substituiOcorrencia(frase, /logico:/g, 'bool');
-    return frase;
+var adicionaTiposDeVariaveis = function (texto) {
+    texto = substituiOcorrencia(texto, /inteiro:/g, 'int');
+    texto = substituiOcorrencia(texto, /real:/g, 'float');
+    texto = substituiOcorrencia(texto, /caractere:/g, 'char');
+    texto = substituiOcorrencia(texto, /logico:/g, 'bool');
+    return texto;
 }
 
 /**
  * Função para mudar os atribuidores
  *
  */
-var adicionaAtribuidores = function (frase) {
-    frase = substituiOcorrencia(frase, / = /g, '==');
-    frase = substituiOcorrencia(frase, /<-/g, '=');
-    frase = substituiOcorrencia(frase, /<>/g, '!=');
-    frase = substituiOcorrencia(frase, /E/g, '&&');
-    frase = substituiOcorrencia(frase, /OU/g, '||');
-    frase = substituiOcorrencia(frase, /NAO/g, '!');
-    return frase;
+var adicionaAtribuidores = function (texto) {
+    texto = substituiOcorrencia(texto, / = /g, '==');
+    texto = substituiOcorrencia(texto, /<-/g, '=');
+    texto = substituiOcorrencia(texto, /<>/g, '!=');
+    texto = substituiOcorrencia(texto, /E/g, '&&');
+    texto = substituiOcorrencia(texto, /OU/g, '||');
+    texto = substituiOcorrencia(texto, /NAO/g, '!');
+    return texto;
 }
 
-var removerFrasesInutesis = function (frase) {
-    frase = substituiOcorrencia(frase, /{.*}/g, '');
-    return frase;   
+/**
+ * Função para retirar partes que não serão úteis
+ *
+ */
+var removerFrasesInutesis = function (texto) {
+    texto = substituiOcorrencia(texto, /{.*}/g, '');
+    return texto;   
 }
